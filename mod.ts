b7b81f940c81d3fde9a2b220f72ff6905751e05b
MierenManz <christiaanmens@gmail.com>
2021-04-12 09:52:05 +0200

    add space

--- mod.ts
+++ mod.ts
@@ -2,3 +2,4 @@ export { config } from "./config.ts";
 
 export * as harmony from "./libs/harmony/mod.ts";
 export * as discordeno from "./libs/discordeno/mod.ts";
+export * as space from "./libs/space/mod.ts";
