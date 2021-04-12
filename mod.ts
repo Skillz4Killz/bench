25c32310d7d0bfd46bd9b0d89408f3a3c5d5ede9
MierenManz <christiaanmens@gmail.com>
2021-04-12 17:42:16 +0200

    export types

--- mod.ts
+++ mod.ts
@@ -3,3 +3,5 @@ export { config } from "./config.ts";
 export * as harmony from "./libs/harmony/mod.ts";
 export * as discordeno from "./libs/discordeno/mod.ts";
 export * as space from "./libs/space/mod.ts";
+
+export * from "./types.ts";
